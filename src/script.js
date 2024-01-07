9390cc0454b48467cec0abd4de9cb047ad5ed77d
Daniel Larrosa <larrosa.dan@gmail.com>
2024-01-07 20:41:03 +0100

    Regularitzant el projecte esborrant node modules.

---
+++ src/script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const searchBar = document.getElementById('search-bar');
+    const searchIcon = document.getElementById('search-icon');
+    const closeSearch = document.getElementById('close-search');
+  
+    menuIcon.addEventListener('click', function () {
+      searchBar.style.display = (searchBar.style.display === 'block') ? 'none' : 'block';
+    });
+  
+    searchIcon.addEventListener('click', function () {
+      searchBar.style.display = 'block';
+    });
+  
+    closeSearch.addEventListener('click', function () {
+      searchBar.style.display = 'none';
+    });
+  });
+  
