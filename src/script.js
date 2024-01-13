bd547507a59568e649a8e396476d9bddc39fcdd1
Daniel Larrosa <larrosa.dan@gmail.com>
2024-01-13 20:46:53 +0100

    Menu superior canvis en versio escriptori

--- src/script.js
+++ src/script.js
@@ -1,19 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuIcon = document.getElementById('menu-icon');
-    const menuMovil = document.getElementById('menu-movil');
-    const searchIcon = document.getElementById('search-icon');
-    const closeSearch = document.getElementById('close-search');
-  
-    menuIcon.addEventListener('click', function () {
-      menuMovil.style.display = (menuMovil.style.display === 'block') ? 'none' : 'block';
-    });
-  
-    searchIcon.addEventListener('click', function () {
-      menuMovil.style.display = 'block';
-    });
-  
-    closeSearch.addEventListener('click', function () {
-      menuMovil.style.display = 'none';
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.getElementById("menu-icon");
+  const menuMovil = document.getElementById("submenu-movil");
+  const searchIcon = document.getElementById("search-icon");
+  const closeSearch = document.getElementById("tancar-cerca");
+
+  menuIcon.addEventListener("click", function () {
+    if (window.innerWidth <= 768) {
+      menuMovil.style.display =
+        menuMovil.style.display === "block" ? "none" : "block";
+    }
   });
-  
+
+  searchIcon.addEventListener("click", function () {
+    if (window.innerWidth <= 768) {
+      menuMovil.style.display = "block";
+    }
+  });
+
+  closeSearch.addEventListener("click", function () {
+    if (window.innerWidth <= 768) {
+      menuMovil.style.display = "none";
+    }
+  });
+});
