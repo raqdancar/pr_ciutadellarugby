e54d58645074da0eaf2f9e828a41eab1722ec47c
Daniel Larrosa <larrosa.dan@gmail.com>
2024-01-14 11:29:04 +0100

    Menu superior acabat, submenu i transicions.

--- src/script.js
+++ src/script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuIcon = document.getElementById("menu-icon");
   const menuMovil = document.getElementById("submenu-movil");
-  const searchIcon = document.getElementById("search-icon");
   const closeSearch = document.getElementById("tancar-cerca");
+  const espaiSociLink = document.getElementById("espai-soci-link");
+  const submenuEscriptori = document.getElementById("submenu-escriptori");
 
   menuIcon.addEventListener("click", function () {
     if (window.innerWidth <= 768) {
@@ -11,15 +12,28 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-  searchIcon.addEventListener("click", function () {
-    if (window.innerWidth <= 768) {
-      menuMovil.style.display = "block";
-    }
-  });
-
   closeSearch.addEventListener("click", function () {
     if (window.innerWidth <= 768) {
       menuMovil.style.display = "none";
     }
+  });
+
+  espaiSociLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    submenuEscriptori.style.display =
+      submenuEscriptori.style.display === "block" ? "none" : "block";
+  });
+
+  document.getElementById("Icons").addEventListener("click", function () {
+    document.getElementById("Icons").addEventListener("click", function () {
+      var menu = document.getElementById("menu-escriptori");
+      menu.classList.toggle("show");
+    });
+  });
+  document.getElementById("menu-icon").addEventListener("click", function () {
+    document.getElementById("Icons").addEventListener("click", function () {
+      var menu = document.getElementById("menu-escriptori");
+      menu.classList.toggle("show");
+    });
   });
 });
